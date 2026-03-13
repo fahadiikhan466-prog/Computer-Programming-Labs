4ad52ca8097e5840e1e82730a644056f314402e9
fahadiikhan466-prog <fahadiikhan466@gmail.com>
2026-03-13 21:46:47 +0500

    Add files via upload

---
+++ Lab 2/Lab 2 task/Age_Input.cpp
@@ -0,0 +1,23 @@
+                                // Lab 2 Task 4.cpp
+                                // Name: [Fahad Ali Shah]
+                                // Registration Number: [BF25NWELE0700]
+
+
+
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    // Variable declaration
+    int a;
+
+    //Basic output
+    cout << "Enter Your Age;" << endl;
+
+    //Basic input
+    cin >> a;
+    
+    return 0;
+}
+
---
+++ Lab 2/Lab 2 task/Print_Name_10_Times.cpp
@@ -0,0 +1,29 @@
+							   // Lab 2 Task 5.cpp
+							   // Name: [Fahad Ali Shah]
+							   // Registration Number: [BF25NWELE0700]
+
+
+
+//print your name 10 times 
+
+#include <iostream>
+using namespace std;
+
+int main()
+	{
+	
+    cout<<" Zawar Shah "<<endl;
+	cout<<" Zawar Shah "<<endl;
+	cout<<" Zawar Shah "<<endl;
+	cout<<" Zawar Shah "<<endl;
+	cout<<" Zawar Shah "<<endl;
+	cout<<" Zawar Shah "<<endl;
+	cout<<" Zawar Shah "<<endl;
+	cout<<" Zawar Shah "<<endl;
+	cout<<" Zawar Shah "<<endl;
+	cout<<" Zawar Shah "<<endl;	
+		
+	return 0;
+}
+
+
