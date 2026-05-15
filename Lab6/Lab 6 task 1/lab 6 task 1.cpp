f93d3cf41f10bcf82345f44c5eaa42524d07b8ab
fahadiikhan466-prog <fahadiikhan466@gmail.com>
2026-05-15 14:18:54 +0500

    Add files via upload

---
+++ Lab6/Lab 6 task 1/lab 6 task 1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i = 1;   // starting value
+
+    while (i <= 10)   // condition
+    {
+        cout << i << endl;   // print number
+        i++;   // increment
+    }
+
+    return 0;
+}
